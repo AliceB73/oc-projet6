93f270dc4ff76d5c42e7a9987d276f15eadebc96
Al <alicemilkovich63@gmail.com>
2023-05-30 12:26:01 +0200

    début du dev

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -2,7 +2,8 @@
 
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
 function theme_enqueue_styles(){
-    // Chargement du style.css du thème parent Twenty Twenty-Three
+    // Chargement du style.css du thème parent Twenty Twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
---
+++ wp-content/themes/planty/index.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+if (have_posts()) : while (have_posts()) : the_post();
+        get_template_part('entry');
+        comments_template();
+    endwhile;
+endif;
+get_template_part('nav', 'below');
+get_footer();
