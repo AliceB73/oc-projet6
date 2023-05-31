ef5956194367c5ea2620bdea1e93f2d7bb732159
Al <alicemilkovich63@gmail.com>
2023-05-31 12:20:26 +0200

    header + footer

--- wp-content/themes/planty/footer.php
+++ wp-content/themes/planty/footer.php
@@ -2,9 +2,8 @@
 <?php get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-    <div id="copyright">
-        &copy; <?php echo esc_html(date_i18n(__('Y', 'blankslate'))); ?> <?php echo esc_html(get_bloginfo('name')); ?>
-    </div>
+    <div id="mentions-legales">
+        <a href="#">Mentions légales</a>
 </footer>
 </div>
 <?php wp_footer(); ?>
--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -2,8 +2,8 @@
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta name="viewport" content="width=device-width" />
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
     <link href="wp-content\themes\planty\style.css" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
@@ -12,26 +12,18 @@
     <?php wp_body_open(); ?>
     <div id="wrapper" class="hfeed">
         <header id="header" role="banner">
-            <div id="branding">
-                <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-                    <?php
-                    if (is_front_page() || is_home() || is_front_page() && is_home()) {
-                        echo '<img class="logo-planty" src="wp-content\themes\planty\assets\Logo.png"';
-                    }
-                    echo '<a href="' . esc_url(home_url('/')) . '" title="' . esc_attr(get_bloginfo('name')) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html(get_bloginfo('name')) . '</span></a>';
-                    if (is_front_page() || is_home() || is_front_page() && is_home()) {
-                        echo '>';
-                    }
-                    ?>
-                </div>
-                <div id="site-description" <?php if (!is_single()) {
-                                                echo ' itemprop="description"';
-                                            } ?>><?php bloginfo('description'); ?></div>
+            <div class="header">
+                <img src="wp-content\themes\planty\assets\Logo.png" class="logo-planty">
+                <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                    <?php wp_nav_menu(array(
+                        'theme_location' => 'main-menu',
+                        'container' => false,
+                        'menu_class' => 'navbar',
+                        'link_before' => '<span itemprop="name">',
+                        'link_after' => '</span>'
+                    )); ?>
+                </nav>
             </div>
-            <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-                <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
-                <div id="search"><?php get_search_form(); ?></div>
-            </nav>
         </header>
         <div id="container">
             <main id="content" role="main">
