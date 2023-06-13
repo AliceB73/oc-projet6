972228456e4973bdc6d4a6fd79c9ba4b03bb6cb5
Al <alicemilkovich63@gmail.com>
2023-06-13 12:41:41 +0200

    Ajout du hook

--- wp-content/themes/plantwenty/functions.php
+++ wp-content/themes/plantwenty/functions.php
@@ -18,3 +18,35 @@ add_action('after_setup_theme', 'montheme_supports');
 
 // Désactivation de l'ajout automatique des balise p et br par Contact Form 7
 add_filter('wpcf7_autop_or_not', '__return_false');
+
+// Ajout du lien d'administration lorsque l'utilisateur est connecté
+
+/*function plantwenty_admin_link($items, $args)
+{
+    if (is_user_logged_in()) {
+        echo '<li><a href="' . get_admin_url() . '" id="adminId">Admin</a></li>';
+    }
+    return $items;
+}*/
+
+function plantwenty_admin_link($items, $args)
+{
+    // on vérifie si l'utilisateur est connecté
+    if (is_user_logged_in()) {
+
+        // on crée le code HTML qu'on stock dans une variable
+        $admin_nav_link = '<li><a href="' . get_admin_url() . '" id="adminId">Admin</a></li>';
+
+        // on divise $items avec preg_split() qu'on stock dans un tableau
+        $array_nav = preg_split('/<\/li>/', $items);
+
+        // on insère $admin_nav_link à l'index 1 dans le tableau
+        array_splice($array_nav, 1, 0, array($admin_nav_link));
+
+        // on convertit le tableau en HTML avec implode()
+        $items = implode('</li>', $array_nav) . '</li>';
+    }
+    return $items;
+}
+
+add_filter('wp_nav_menu_items', 'plantwenty_admin_link', 10, 2);
